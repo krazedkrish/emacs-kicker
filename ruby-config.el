4e3dbd28980dad9ff0f3e06b3409b65dbceea424
krazedkrish <krazedkrish@gmail.com>
2014-12-28 19:44:49 +0545

    coffee script indent to 2 spaces

--- ruby-config.el
+++ ruby-config.el
@@ -66,3 +66,12 @@
 
 ;; for flymake-haml
 (add-hook 'haml-mode-hook 'flymake-haml-load)
+
+
+;; for coffee script
+;; This gives you a tab of 2 spaces
+(custom-set-variables '(coffee-tab-width 2))
+;; automatically clean up bad whitespace
+(setq whitespace-action '(auto-cleanup))
+;; only show bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab))
