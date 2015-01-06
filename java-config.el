c6ad26de0e72068b2532bd904f5c94b98bfe6e34
krazedkrish <krazedkrish@gmail.com>
2015-01-06 20:08:16 +0545

    load old jdee in new emacs

--- java-config.el
+++ java-config.el
@@ -1,3 +1,13 @@
+(setq jde-check-version-flag nil)
+(define-obsolete-function-alias 'make-local-hook 'ignore "21.1")
+(unless (fboundp 'semantic-format-prototype-tag-java-mode)
+  (defalias 'semantic-format-prototype-tag-java-mode 'semantic-format-tag-prototype-java-mode))
+  (require 'hippie-exp)
+
+(autoload 'jde-mode "jde" "JDE mode." t)
+  (setq auto-mode-alist 
+        (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
+
 (progn (setq android-ndk-root-path "/share/android-sdk-linux/android-ndk-r10d")
     (setq android-sdk-root-path "/share/android-sdk-linux"))
 (setq android-default-package "com.zxy")
