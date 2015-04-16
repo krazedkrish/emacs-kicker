e226ac3049df88c43d4052ca94930b5aca635150
krazedkrish <krazedkrish@gmail.com>
2015-04-16 10:05:10 +0545

    add mouse actions to hightlight-symbol

--- init.el
+++ init.el
@@ -71,6 +71,7 @@
    ;; powerline				; emacs-powerline
    git-gutter                           ;highlight git changes
    projectile                           ;project interaction library
+   smooth-scroll  ; smooth scroll
    
    ;; python special packages
    
@@ -210,10 +211,17 @@
 (setq projectile-switch-project-action 'neotree-projectile-action)
 
 ;; highlight-sysmbol configurations
-    (global-set-key [(control f3)] 'highlight-symbol-at-point)
-    (global-set-key [f3] 'highlight-symbol-next)
-    (global-set-key [(shift f3)] 'highlight-symbol-prev)
-    (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+(global-set-key [(control shift f3)] 'unhighlight-regexp)
+(global-set-key [(control shift mouse-1)]
+                (lambda (event)
+                  (interactive "e")
+                  (save-excursion
+                    (goto-char (posn-point (event-start event)))
+                    (highlight-symbol-at-point))))
 
 
 ;; tabbar-ruler configuration
