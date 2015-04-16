b331ff357963f5c7b387cecaef9cb78710818c5a
krazedkrish <krazedkrish@gmail.com>
2015-04-16 10:25:34 +0545

    corrects .erb to use web-mode

--- init.el
+++ init.el
@@ -317,7 +317,7 @@
 
 ;; enable web-mode for html and template engines
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
