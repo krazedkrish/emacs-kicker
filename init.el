1bdcdb85fb1e13805c847108d52d8b6577d3fde6
krazedkrish <krazedkrish@gmail.com>
2015-04-27 19:03:05 +0545

    disable tabbar because it is heavy

--- init.el
+++ init.el
@@ -225,13 +225,13 @@
 
 
 ;; tabbar-ruler configuration
-  (setq tabbar-ruler-global-tabbar t) ; If you want tabbar
+;;  (setq tabbar-ruler-global-tabbar t) ; If you want tabbar
 ;;  (setq tabbar-ruler-global-ruler t) ; if you want a global ruler
 ;;  (setq tabbar-ruler-popup-menu t) ; If you want a popup menu.
 ;;  (setq tabbar-ruler-popup-toolbar t) ; If you want a popup toolbar
 ;;  (setq tabbar-ruler-popup-scrollbar t) ; If you want to only show the
                                         ; scroll bar when your mouse is moving.
-  (require 'tabbar-ruler)
+;;  (require 'tabbar-ruler)
 
 ;; for minimap
 ;; ###autoload
