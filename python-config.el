8aaed616491b60c3fa7bc6a82ca141b684676cf4
krazedkrish <krazedkrish@gmail.com>
2015-09-15 07:22:16 +0545

    change tabwidth to 2

--- python-config.el
+++ python-config.el
@@ -5,3 +5,17 @@
 ;; jedi configs for autocomplete
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)                 ; optional
+
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(coffee-tab-width 2)
+'(web-mode-code-indent-offset 2)
+'(web-mode-css-indent-offset 2)
+'(web-mode-markup-indent-offset 2)
+'(web-mode-style-padding 2)
+'(web-mode-script-padding 2)
+)
+
